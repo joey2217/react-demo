533d0fad4b6b6919a258e288238190565a67079c
Joey <786678257@qq.com>
2023-03-15 16:09:53 +0800

    chore: build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom'],
-          antd: ['antd'],
-        },
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: {
+  //         react: ['react', 'react-dom', 'react-router-dom'],
+  //         // antd: ['antd'],
+  //       },
+  //     },
+  //   },
+  // },
   server: {
     proxy: {
       '/api': {
